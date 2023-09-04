0080519d630ff8ff3fd7cd64d42c736d6304b872
Viktor Velychko <Cezn.Vi@gmail.com>
2023-09-04 22:08:06 +0300

    W I P

--- SITE/script.js
+++ SITE/script.js
@@ -1,26 +1,21 @@
-var getSummElementsArray = function (arr) {
-    var summ = 0;
+function func(arr) {
+    let summm = 0;
 
-    if (Array.isArray(arr)) {
-        for (let i = 0; i < arr.length; i++) {
-            if (Array.isArray(arr[i])) {
-                summ = getSummElementsArray(arr[i]);
+    if(Array.isArray(arr)){
+        for(let i=0; i < arr.length; i++){
+            if(Array.isArray(arr[i])){
+                summm += func(arr[i])
             }
-            else {
-                if (typeof arr[i] === 'number') {
-                    summ += arr[i];
-                }
+            else if(typeof arr[i] == 'number' && !isNaN(arr[i])){
+                summm += arr[i];
             }
-
         }
     }
-    else if(typeof arr === 'number') {
-        summ = arr;
+    else if(typeof arr == 'number' && !isNaN(arr)){
+        summm = arr;
     }
-
-
-    return summ;
+    return summm;
 }
-//Реализоавть функцию которая вернет сумму элементов массива любого уровня вложенности
-console.log(getSummElementsArray(["sdads", 45, null, -342, [NaN, "sdsda", 456], ['fds', [123, 65, null, 1231]]]))
-console.log(getSummElementsArray(["ВАся", 45, null, -658, [NaN, "sdfsdf", 456], ['fd', [124, 658, null, 466]]]));
+
+console.log(func(["ВАся", 45, null, -658, [NaN, "sdfsdf", 456], ['fd', [124, 658, null, 466]]]));
+console.log("Верный ответ: 1091");
